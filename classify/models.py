de47bb1dc7055b91a0133af0f13f3b41e3df6231
Covenant Monday <covenantmonday863@gmail.com>
2026-04-19 22:18:25 +0100

    add initial Django app structure with models, serializers, and admin configuration

---
+++ classify/models.py
@@ -0,0 +1,19 @@
+from random import sample
+
+from django.db import models
+import uuid
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    gender_probability = models.FloatField()
+    sample_size = models.IntegerField()
+    age = models.IntegerField()
+    age_group = models.CharField(max_length=20)
+    country_id = models.CharField(max_length=10) 
+    country_probability = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
