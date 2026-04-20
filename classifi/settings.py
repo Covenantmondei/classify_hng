865d8918739bc2bb3356d0a9e6061cfa3c667ea4
Covenant Monday <covenantmonday863@gmail.com>
2026-04-20 09:19:35 +0100

    update database configuration to use DATABASE_URL environment variable

--- classifi/settings.py
+++ classifi/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'classifi.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_POSTGRES_URL'),
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
